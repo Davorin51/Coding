045242a657533d5fc8492b4ba0173c29c7caa2ea
Davorin51 <72658502+Davorin51@users.noreply.github.com>
2020-10-24 17:19:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+
+            Triangle equal = new Triangle(3);
+
+            Console.WriteLine(equal.A);
+            Console.WriteLine(equal.B);
+            Console.WriteLine(equal.C);
+
+
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get
+            {
+                return this.a;
+            }
+            set
+            {
+                this.a = value;
+            }
+        }
+        public double C
+        {
+            get
+            {
+                return this.c;
+            }
+            set
+            {
+                this.c = value;
+            }
+        }
+        public double B
+        {
+            get
+            {
+                return this.b;
+            }
+            set
+            {
+                this.b = value;
+            }
+        }
+
+
+        public Triangle()
+        {
+            A = B = C = 1;
+        }
+        public Triangle(int a)
+        {
+            A = B = C = a;
+        }
+        public Triangle(int a, int b) { A = a; B = C = b; }
+
+    }
+}
